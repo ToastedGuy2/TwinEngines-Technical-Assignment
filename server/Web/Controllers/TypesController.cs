1ae3e5d15b2901bd331e85e7d8ad5e85568ebeb6
order-66 <toastedguy2@protonmail.com>
2021-12-18 19:24:47 -0600

    Types Api Done

---
+++ server/Web/Controllers/TypesController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Entities;
+using Services;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TypesController : ControllerBase
+    {
+        private readonly ICustomerTypeService _typeService;
+
+        public TypesController(ICustomerTypeService typeService)
+        {
+            this._typeService = typeService;
+        }
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<CustomerType>>> GetCustomerType()
+        {
+            var types = await _typeService.GetAllAsync();
+
+            return Ok(types);
+        }
+
+    }
+}
