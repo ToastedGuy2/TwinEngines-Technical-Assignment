aa4c0d9f48c5cc5365c4b9a639e1285246317b2c
order-66 <toastedguy2@protonmail.com>
2021-12-19 11:28:36 -0600

    Theme ready

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@mui/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
 import { CssBaseline } from "@mui/material";
